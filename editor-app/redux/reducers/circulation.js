be1da7cd58dc2885c8163f81f0af5a7b5554f8e4
luojie.5408@163.com <as56210>
2017-12-19 19:29:52 +0800

    改名

--- editor-app/redux/reducers/circulation.js
+++ editor-app/redux/reducers/circulation.js
@@ -6,31 +6,33 @@ const uniqAdd = (data, item) => {
     data.push(item)
     return data
 }
-export default reduceWrap('Service task', {}, (state, action, ind) => {
+export default reduceWrap('Circulation task', {}, (state, action, ind) => {
     let data = fromJS(state)
     switch (action.type) {
-        case 'service/previousNodeSpecifiedChange':
-            if (ind == 'not exist') {
-                const newCreate = fromJS({ id: state.id, data: [],previousNodeSpecified:true})
-                return data.updateIn(['repo'], 'initial', (el) => el.push(newCreate)).toJS()
-            }
-            return data.updateIn(['repo', ind, 'previousNodeSpecified'], false, (el) => !el).toJS()
-
-        case 'service/withdrawChange':
-            return data.updateIn(['repo', ind, 'withdraw'], false, (el) => !el).toJS()
-
-        case 'service/init':
-            return data.updateIn(['repo'], [], (el) => {
-                return el.push(fromJS(action.data))
-            }).toJS()
-        case 'service/newNodeInit':
+        case 'circulation/newNodeInit':
             if (ind == 'not exist') {
                 action.init()                
                 const newCreate = fromJS({ id: state.id, data: []})
                 return data.updateIn(['repo'], 'initial', (el) => el.push(newCreate)).toJS()
             }
             return state
-        case 'service/addRole':  //pushApproveList
+
+
+        case 'circulation/previousNodeSpecifiedChange':
+            if (ind == 'not exist') {
+                const newCreate = fromJS({ id: state.id, data: [],previousNodeSpecified:true})
+                return data.updateIn(['repo'], 'initial', (el) => el.push(newCreate)).toJS()
+            }
+            return data.updateIn(['repo', ind, 'previousNodeSpecified'], false, (el) => !el).toJS()
+
+        // case 'circulation/withdrawChange':
+        //     return data.updateIn(['repo', ind, 'withdraw'], false, (el) => !el).toJS()
+
+        case 'circulation/init':
+            return data.updateIn(['repo'], [], (el) => {
+                return el.push(fromJS(action.data))
+            }).toJS()
+        case 'circulation/addRole':  //pushApproveList
             if (ind == 'not exist') {
                 const newCreate = fromJS({ id: state.id, data: [action.item] })
                 return data.updateIn(['repo'], 'initial', (el) => el.push(newCreate)).toJS()
@@ -39,11 +41,11 @@ export default reduceWrap('Service task', {}, (state, action, ind) => {
             return data.updateIn(['repo', ind], 'initial', (el) => {
                 return el.set('data', fromJS(uniqAdd(poolData, action.item)))
             }).toJS()
-        case 'service/deleteRole':
+        case 'circulation/deleteRole':
             return data.updateIn(['repo', ind], 'initial', (el) => {
                 return el.set('data', el.get('data').delete(action.index))
             }).toJS()
-        case 'service/clear':
+        case 'circulation/clear':
             return data.updateIn(['repo', ind, 'data'], [], (el) => fromJS([])).toJS()
 
         default:
