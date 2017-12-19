85ea1d03f69a52f4a182344e449481274ed0e590
luojie.5408@163.com <as56210>
2017-12-19 19:33:02 +0800

    subflow 保存主逻辑 文件

--- editor-app/ngEvent/save/subflow.js
+++ editor-app/ngEvent/save/subflow.js
@@ -1,12 +1,19 @@
 import rolesJsonSpeller from './rolesJsonSpeller'
 
 export default function(canvas) {
+    let returnFlag = true
+
     const subflow = window.reduxStore.getState().subflow
+
+    const procDefKey = window.getQueryString("pid")
+    const versionId = window.getQueryString("versionId")
+    const sub_properties = { versionId, procDefKey, subSettings: [] }
+
     subflow.repo.forEach((repoObj) => {
         let currentElement = canvas.getChildShapeByResourceId(repoObj.id)
         if (repoObj.id && !currentElement) return
         let jsonArray = []
-        // jsonArray = rolesJsonSpeller(jsonArray, repoObj.data)
+        jsonArray = rolesJsonSpeller(jsonArray, repoObj.data)
         const subProcessSet = {
             "subProcessSet": [repoObj.subProcess]
         }
@@ -27,34 +34,114 @@ export default function(canvas) {
                 }
             ]
         }
-/*        sub_properties: {
-            "versionId": "1",
-            "procDefKey": "Pro_9d5ce17c497d497ba81f6a5b9a1ea272",
-            "subSettings": [{
-                    "subVersionId": "1",
-                    "subProcDefKey": "Pro_8e9a0d771ccd45d38af0f1a2e954f7a2",
-                    "actId": "sid-B16F8C26-2B60-466F-A872-21DE598A908B",
-                    "candidateUsers": "{\"candidateOwners\" : [{\"cate\" : \"fromDb\",\"id\" : \"1\",\"text\" : \"从DB中获取\",\"value\" : \"fromDb(1)\"}]}",
-                    "startType": "One",
-                    "startUserSource": "Parent",
-                    "startUserValue": "",
-                    "formMapping": "[{\"left\":\"field_1510555486841\",\"right\":\"field_1510296977961\",\"type\":\"text\",\"subForms\":[]},{\"left\":\"field_1510555506743\",\"right\":\"field_1510297609155\",\"type\":\"sub_form\",\"subForms\":[{\"left\":\"field_1510555521126\",\"right\":\"field_1510297625044\",\"type\":\"text\",\"subForms\":[]},{\"left\":\"field_1510555534868\",\"right\":\"field_1510297639134\",\"type\":\"text\",\"subForms\":[]}]}]",
-                    "processType": "Normal"
-                },
-                {
-                    "subVersionId": "1",
-                    "subProcDefKey": "Pro_403b405a36ad4014a411dc8b53ae6e36",
-                    "actId": "sid-B16F8C26-2B60-466F-A872-21DE598A908B",
-                    "candidateUsers": "{\"candidateOwners\" : [{\"cate\" : \"fromDb\",\"id\" : \"1\",\"text\" : \"从DB中获取\",\"value\" : \"fromDb(1)\"}]}",
-                    "startType": "ByApproved",
-                    "startUserSource": "Parent",
-                    "startUserValue": "",
-                    "formMapping": "[{\"left\":\"field_1510555922318\",\"right\":\"field_1510296977961\",\"type\":\"text\",\"subForms\":[]}]",
-                    "processType": "Normal"
+        /* check是否匹配 */
+        if(
+            repoObj.leftFields.some((el, ind) => {
+                if(!repoObj.subRights){
+                    window.showAlert('子表单尚未选择')
+                    returnFlag = false
+                    return true
                 }
-            ]
-        }*/
-        // currentElement.setProperty('objData', jsonArray)
+
+                if(el.type != 'sub_form') return false
+                
+                if(!repoObj.subRights[el.name]){
+                    window.showAlert('子表单"'+ el.title +'"尚未匹配')
+                    returnFlag = false
+                    return true
+                }else{
+                    if(!repoObj.subRights[el.name].rightFormId){
+                        window.showAlert('子表单"'+ el.title +'"尚未匹配')
+                        returnFlag = false
+                        return true   
+                    }
+                }
+            })
+        ){
+            return 
+        }
+
+
+
+
+        const formMapping = repoObj.leftFields.map((el, ind) => {
+            
+            if (el.type != "sub_form") {
+                if(el.require){
+                    if(!repoObj.mainRight){
+                        window.showAlert('子表单尚未选择')
+                        returnFlag = false
+                        return 
+                    }
+                    if(!repoObj.mainRight[el.name]){
+                        window.showAlert('必填项"'+ el.title +'"尚未匹配字段')
+                        returnFlag = false
+                        return 
+                    }
+                    if(repoObj.mainRight[el.name].name == 'initial'){
+                        window.showAlert('必填项"'+ el.title +'"尚未匹配字段')
+                        returnFlag = false
+                        return 
+                    }
+                }
+                const returnObj = {
+                    left: el.name,
+                    right: repoObj.mainRight && repoObj.mainRight[el.name] && repoObj.mainRight[el.name].name||'',
+                    type: el.type,
+                    subForms: []
+                }
+                return returnObj
+
+            }
+
+
+
+
+
+            if (el.type == "sub_form") {
+                const returnObj = {
+                    left: el.name,
+                    right: repoObj.subRights[el.name].rightFormId,
+                    type: el.type,
+                    subForms: []
+                }
+
+                const optionMap = repoObj.subRights[el.name].map
+                const subForms = []
+                for(let v in optionMap){
+                    if(optionMap.hasOwnProperty(v)){
+                        subForms.push({
+                            left: v,
+                            right: optionMap[v].value,
+                            type: optionMap[v].type||'尚未指派',
+                            subForms: []
+                        })
+                    }
+                }
+                returnObj.subForms = subForms
+                return returnObj
+            }
+        })
+
+        debugger
+        const subSetting = {
+            "subVersionId": repoObj.subProcess.versionId||'',
+            "subProcDefKey": repoObj.subProcess.subProcDefKey,
+            "actId": currentElement.resourceId,
+            "startType": repoObj.isOne ? "One" : "ByApproved",
+
+            "candidateUsers": JSON.stringify(jsonArray), //"{\"candidateOwners\" : [{\"cate\" : \"fromDb\",\"id\" : \"1\",\"text\" : \"从DB中获取\",\"value\" : \"fromDb(1)\"}]}",
+            "formMapping":JSON.stringify(formMapping), // "[{\"left\":\"field_1510555486841\",\"right\":\"field_1510296977961\",\"type\":\"text\",\"subForms\":[]},{\"left\":\"field_1510555506743\",\"right\":\"field_1510297609155\",\"type\":\"sub_form\",\"subForms\":[{\"left\":\"field_1510555521126\",\"right\":\"field_1510297625044\",\"type\":\"text\",\"subForms\":[]},{\"left\":\"field_1510555534868\",\"right\":\"field_1510297639134\",\"type\":\"text\",\"subForms\":[]}]}]",
+
+            "startUserSource": "Parent",
+            "startUserValue": "",
+            "processType": "Normal"
+        }
+
+        sub_properties.subSettings.push(subSetting)
+
+        window.sub_properties = sub_properties
+        
         currentElement.setProperty('servicetaskexpression', "")
         currentElement.setProperty('servicetaskfields', value)
         currentElement.setProperty('reduxData', repoObj)
@@ -62,8 +149,8 @@ export default function(canvas) {
         currentElement.setProperty('classify', "SubProcess")
 
         // if(repoObj.previousNodeSpecified){
-            // currentElement.setProperty('previousNodeSpecified', true)
+        // currentElement.setProperty('previousNodeSpecified', true)
         // }
     })
-
+    return returnFlag
 }
