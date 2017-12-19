a25877464252302951361765b8ff1b8da9c4e8fb
luojie.5408@163.com <as56210>
2017-12-19 19:34:21 +0800

    抽象出公共组件 牛逼的做法  connect2redux

--- editor-app/react-src/utils/connect2redux.js
+++ editor-app/react-src/utils/connect2redux.js
@@ -4,7 +4,7 @@ global.connect2redux = (reduceName,reactComp) => {
         const repo = state[reduceName].repo
         const id = state[reduceName].id
         const currentRepo = repo.filter((el,index)=>el.id == id) 
-        if(currentRepo.length==0) return false
+        if(currentRepo.length==0) return { currentRepo: false } 
         return { currentRepo: currentRepo[0] } 
     }
     const mapDispatchToProps = (dispatch) => ({dispatch})
