9a5b3343febf3d301d0cfb41f8f3c05cf50f843c
luojie.5408@163.com <as56210>
2017-12-21 14:58:05 +0800

    便捷写法

--- editor-app/ngEvent/save/approve.js
+++ editor-app/ngEvent/save/approve.js
@@ -3,11 +3,10 @@ export default function(canvas) {
     window.reduxStore.getState().approve.repo.forEach((repoObj) => {
         let currentElement = canvas.getChildShapeByResourceId(repoObj.id)
         if (repoObj.id && !currentElement) return
-        let jsonArray = []
-        jsonArray = rolesJsonSpeller(jsonArray,repoObj.data)
+        
         let value = {
             "assignment": {
-                "candidateOwners": jsonArray
+                "candidateOwners": rolesJsonSpeller([],repoObj.data)
             }
         }
         currentElement.setProperty('usertaskassignment', value)
