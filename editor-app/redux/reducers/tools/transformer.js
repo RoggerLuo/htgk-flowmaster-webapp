f6112a806a85bec0b9a6b00aa34b40a0ec1da10c
luojie.5408@163.com <as56210>
2017-12-26 12:02:14 +0800

    reducer transformer bug修复, 增加touch、add的默认值为List等

--- editor-app/redux/reducers/tools/transformer.js
+++ editor-app/redux/reducers/tools/transformer.js
@@ -1,21 +1,23 @@
 import { toJS, fromJS, List, Map } from 'immutable'
 
-const replaceFunction = (op) => {
+const updaterGenerator = (op, item) => {
+    item = fromJS(item)
     switch (op) {
         case 'replace':
             return (el) => item
         case 'add':
             return (el) => {
                 if (!List.isList(el)) throw new  Error('不是数组 不能使用add operation')
-                el.push(item)
+                return el.push(item)
             }
         case 'delete':
+            Error('delete未实现')
             return (el) => el.push(item)
         default:
             throw new  Error('你传的是什么鬼operation参数？？')
     }
 }
-const checkType = (type) => {
+const typeValidate = (type, item) => {
     if (type) { // 传入type的话，就自动校验, 针对数据结构复杂的情况
         switch (type) {
             case 'array':
@@ -34,13 +36,19 @@ const checkType = (type) => {
         }
     }
 }
-export default function(data) {
+
+export default function(data, ind) {
     return (op, path, item, type) => {
+        if(op == 'touch') return data.toJS()
+        typeValidate(type, item)
+
+        const updater = updaterGenerator(op, item)
+
+        const dataPath = ['repo', ind]
         if (!(path instanceof Array)) throw new  Error('Whoops! 输入的path不是数组，脑子秀逗了吧')
-        checkType(type)
-        item = fromJS(item)
-        const dataPath = ['repo']
         dataPath.push(...path)
-        return data.updateIn(dataPath, '', replaceFunction(op)).toJS() //fromJS(initialValue)
+
+        if(op == 'add') return data.updateIn(dataPath, List(), updater).toJS()            
+        return data.updateIn(dataPath, '', updater).toJS()
     }
 }
