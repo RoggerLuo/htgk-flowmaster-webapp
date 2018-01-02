ba9b1eba2bf070e997044c7b3466ad404362fdb5
luojie.5408@163.com <as56210>
2018-01-02 10:26:59 +0800

    连线未命名  增加定位提示

--- editor-app/ngEvent/save/isSfsUnnamed.js
+++ editor-app/ngEvent/save/isSfsUnnamed.js
@@ -4,8 +4,10 @@ export default ($scope) => {
     return json.childShapes.some((el, index) => {
         switch (el.stencil.id) {
             case 'SequenceFlow':
-                if (!!el.properties["name"] ) {
-                    window.showAlert(`连线未命名`) 
+                if (!el.properties["name"] ) {
+                    const resourceId = el.outgoing && el.outgoing[0] && el.outgoing[0].resourceId || false
+                    const nextElement = windowCanvas.getChildShapeByResourceId(resourceId)
+                    window.showAlert(`连接“${nextElement.properties["oryx-name"]}”节点的连线未命名`) 
                     return true
                 }
                 break
