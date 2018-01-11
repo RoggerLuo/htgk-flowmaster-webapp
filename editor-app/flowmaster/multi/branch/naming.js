7e9fcd815f97623c906f1cff6d1ee2504a65811d
luojie.5408@163.com <as56210>
2018-01-11 21:14:13 +0800

    会签连线的bug清除

--- editor-app/flowmaster/multi/branch/naming.js
+++ editor-app/flowmaster/multi/branch/naming.js
@@ -1,28 +1,29 @@
 import autoNaming from './autoNaming'
 
-export function by_gateway(shape) { //这个shape是"Exclusive gateway"
+function by_gateway(shape) { //这个shape是"Exclusive gateway"
     //总共就两根线，都判断一次好了
     const sf = shape.outgoing[0] || false
     if (!sf) return true
-    if (!sf.properties.conditionsequenceflow) autoNaming(sf, shape) //如果第一根线没有设置过
+    if (!sf.properties['oryx-name']) autoNaming(sf, shape) //如果第一根线没有设置过
     //另一根线
     const sf2 = shape.outgoing[1] || false //接着判断第二根线
     if (!sf2) return true
-    if (!sf2.properties.conditionsequenceflow) autoNaming(sf2, shape)
+    if (!sf2.properties['oryx-name']) autoNaming(sf2, shape)
 }
 
-export function by_sf(shape) {
+function by_sf(shape) {
     const prevShape = shape.incoming[0]
-    naming_by_gateway(prevShape)
+    by_gateway(prevShape)
 }
 
-export function by_the_shape_after_sf(shape) {
+function by_the_shape_after_sf(shape) {
     const prevShape = shape.incoming[0]
     if (prevShape) {
         const prevPrevShape = prevShape.incoming[0]
-        if(prevPrevShape){
-            if(fm.multi.identify.isGateway(prevPrevShape)) autoNaming(prevShape)
+        if (prevPrevShape) {
+            if (fm.multi.is.gateway(prevPrevShape)) autoNaming(prevShape)
         }
     }
     return false
 }
+export default { by_sf, by_gateway, by_the_shape_after_sf }
