6ed36ba81e0bdb7e07326bc97367fb3705b4bcef
Roger <luojie.5408@163.com>
2018-02-09 10:03:00 +0800

    见鬼了

--- editor-app/flowmaster/save/nodesValid/subflow/checkSubform.js
+++ editor-app/flowmaster/save/nodesValid/subflow/checkSubform.js
@@ -6,7 +6,10 @@ export default function(repo) {
 
     if (isExistSubform) {
 
-        if(repo.leftFields.filter(el => el.type == 'sub_form').some(el=>el.required)){
+        if(repo.leftFields
+            .filter(el => el.type == 'sub_form')
+            .some(el=>el.required)
+        ){
             if (!repo.subRights) {
                 window.showAlert('必填子表单尚未匹配')
                 returnFlag = false
@@ -14,48 +17,47 @@ export default function(repo) {
             }            
         }
 
-        const formMatchCheckNotPass = repo.leftFields.filter(el => el.type == 'sub_form').some((el, ind) => {
-            
-            // if (el.type != 'sub_form') return false // 如果不是子表单项就跳过, 要取el的children
+        const formMatchCheckNotPass = repo.leftFields
+            .filter(el => el.type == 'sub_form')
+            .some((el, ind) => {
 
-            const subItem = repo.subRights[el.name]
-            
-            if (!subItem) { //如果选项尚未选择过
-
-                if(el.required){
-                    window.showAlert('子表单"' + el.title + '"尚未匹配')
-                    returnFlag = false
-                    return true
-                }else{
-                    return false
-                }
-
-            } else {
+                if(!repo.subRights) return false //如果没选择，就跳过
                 
-                //如果选项 已经选择过
-                if (!subItem.rightFormId) {  //但是为'请选择'(false)
-                    window.showAlert('子表单"' + el.title + '"尚未匹配')
-                    returnFlag = false
-                    return true
-                }
-            }
-            // 表单 对应过了，接下来校验 表单中的每一个字段
-            const subForm = el.children
-            subForm.forEach(el2=>{
-                if(!subItem.map[el2.name]){
-                    window.showAlert('子表单'+el.title+'中，字段"' + el2.title + '"尚未匹配')
-                    returnFlag = false
-                    return true
-                }
-                if(!subItem.map[el2.name].value){
-                    window.showAlert('子表单'+el.title+'中，字段"' + el2.title + '"尚未匹配')
-                    returnFlag = false
-                    return true
-                }
+                const subItem = repo.subRights[el.name]
+                if (!subItem) { //如果选项尚未选择过
 
-            })
-            
+                    if(el.required){
+                        window.showAlert('子表单"' + el.title + '"尚未匹配')
+                        returnFlag = false
+                        return true
+                    }else{
+                        return false
+                    }
 
+                } else {
+                    
+                    //如果选项 已经选择过
+                    if (!subItem.rightFormId) {  //但是为'请选择'(false)
+                        window.showAlert('子表单"' + el.title + '"尚未匹配')
+                        returnFlag = false
+                        return true
+                    }
+                }
+                // 表单 对应过了，接下来校验 表单中的每一个字段
+                const subForm = el.children
+                subForm.forEach(el2=>{
+                    if(!subItem.map[el2.name]){
+                        window.showAlert('子表单'+el.title+'中，字段"' + el2.title + '"尚未匹配')
+                        returnFlag = false
+                        return true
+                    }
+                    if(!subItem.map[el2.name].value){
+                        window.showAlert('子表单'+el.title+'中，字段"' + el2.title + '"尚未匹配')
+                        returnFlag = false
+                        return true
+                    }
+
+                })
         })
     }
 
