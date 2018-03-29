ec08cede81ebff88eebce0de03e2c0393093c5d8
Roger <luojie.5408@163.com>
2018-03-29 14:29:57 +0800

    update

--- editor-app/flowmaster/approveClass/Setting/backToLastControl.jsx
+++ editor-app/flowmaster/approveClass/Setting/backToLastControl.jsx
@@ -1,4 +1,34 @@
-
+export default (data) => {
+    if(
+        fm.previousShape.is('Start event')||
+        fm.previousShape.is('Subflow')||
+        fm.previousShape.is('Parallel gateway')||
+        fm.previousShape.is('Inclusive gateway')||
+        fm.previousShape.is('Circulation task')||
+        start_with_x() ||
+        circ_with_X()
+    ){
+        data = data.filter(el => el.title != '允许退回上一节点审批人')
+    }
+    return data
+}
+function circ_with_X(){
+    let flag = false
+    const looper1 = (sf) => {
+        const shape = fm.getIncoming(sf)
+        if(fm.getTitle(shape) === 'Circulation task'){
+            flag = true
+        }
+    }
+    const looper2 = (sf) => {
+        const shape = fm.getIncoming(sf)
+        if(fm.getTitle(shape) === 'Exclusive gateway'){
+            shape.incoming.forEach(looper1)  
+        }
+    }
+    fm.currentSelectedShape.incoming.forEach(looper2)
+    return flag
+}
 function start_with_x(){
     let flag = false
     const looper1 = fm.incomingLooper('Start event',(shape)=>{
@@ -10,18 +40,3 @@ function start_with_x(){
     fm.currentSelectedShape.incoming.forEach(looper2)
     return flag
 }
-
-export default (data) => {
-    if(
-        fm.previousShape.is('Start event')||
-        fm.previousShape.is('Subflow')||
-        fm.previousShape.is('Parallel gateway')||
-        fm.previousShape.is('Inclusive gateway')||
-        fm.previousShape.is('Circulation task')||
-        start_with_x()
-    ){
-        data = data.filter(el => el.title != '允许退回上一节点审批人')
-    }
-    return data
-}
-
